59c9a2d26241bd42dca30f1be143e8e14979f399
tamilmaranr <108916935+tamilmaranr@users.noreply.github.com>
2023-04-05 15:43:53 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Book from './components/Book'
+import StarRating from './components/StarRating'
+
+function App() {
+  return (
+    <div>
+        <Book/>
+        <StarRating/>
+    </div>
+  )
+}
+
+export default App
